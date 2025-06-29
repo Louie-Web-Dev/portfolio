e1ca340dd87f944896f84dcb53289c0f7728e18d
Louie-Web-Dev <chrstnmrn27@gmail.com>
2025-06-29 19:05:44 +0800

    changes

--- sample/index.php
+++ sample/index.php
@@ -23,15 +23,50 @@
 
 <?php 
 
-require_once 'database.php'; // Assuming database.php is in the sample folder
-    
+require_once 'database.php'; 
+
+     echo "Database connection successful!<br>";
     $name = "John";
-    $age = 30;
+    $location = "New York"; 
     $email = "sample@gmail.com";
+
+    //integer values
+    $age = 30;
+   
+
+    //float values
+    // decimal values
+    $salary = 5.5;
+    $hours_worked = 8.5;
+
+    //boolean values
+    // true or false
+    $employed = true;
+    $online = false;
+    $for_sale = true;
 
 
     echo "Name: $name<br>";
-    echo "Age: $age<br>"; 
+    echo "Location: $location<br>";
     echo "Email: $email<br>";
-    echo "Database connection successful!<br>";
+
+
+    echo "Age: $age<br>"; 
+    
+   
+    echo "Salary per hour \$ $salary<br>";
+
+
+    echo "Employed: {$employed}<br>";
+    echo"online: {$online}<br>";
+    echo"for sale: {$for_sale}<br>";
+
+    echo"you are $name, you are $age years old, you live in 
+    $location";
+
+    $total_salary = $salary * $hours_worked;
+    echo "<br>Total salary for the day: \$ $total_salary<br>";
+
 ?>
+
+
