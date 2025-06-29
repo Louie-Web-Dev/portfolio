10ce554906885a751dcd5f87119c8926f5246cfe
Louie-Web-Dev <chrstnmrn27@gmail.com>
2025-06-29 14:47:37 +0800

    changes

---
+++ sample/index.php
@@ -0,0 +1,33 @@
+<!-- 
+
+    echo "hello world<br>";
+    echo "hello world";
+ -->
+
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">  
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <br>
+
+    <button class="btn">Click Me</button>
+
+</body>
+</html> -->
+
+<?php 
+    $name = "John";
+    $age = 30;
+    $email = "sample@gmail.com";
+
+
+    echo "Name: $name<br>";
+    echo "Age: $age<br>"; 
+    echo "Email: $email<br>";
+?>
